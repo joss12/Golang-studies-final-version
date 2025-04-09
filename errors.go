f19f712f90c58739dc034dcd3bc36f9e0cf69b0e
joss12 <drh374@mail.com>
2025-04-09 18:17:20 -0400

    errors file completed

--- errors.go
+++ errors.go
@@ -5,47 +5,86 @@ import (
 	"fmt"
 )
 
-
-func sqrt (x float64)(float64, error){
-	if x < 0{
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
 		return 0, errors.New("Math error: square root of negative number")
 	}
 	//compute the square root
 	return 1, nil
 }
 
-func process(data []byte)error {
-	if len(data) == 0{
+func process(data []byte) error {
+	if len(data) == 0 {
 		return errors.New(" Empty Data")
 	}
 	//Process data
 	return nil
 }
 
-func main (){
+func main() {
 
 	// result, err := sqrt(16)
 	// if err != nil{
 	// 	fmt.Println(err)
-	// 	return 
+	// 	return
 	// }
 	// fmt.Println(result)
 
 	// result1, err1:= sqrt(-16)
 	// if err1 != nil{
 	// 	fmt.Println(err1)
-	// 	return 
+	// 	return
 	// }
 	// fmt.Println(result1)
 
-	data := []byte{}
+	// data := []byte{}
 	// if err := process(data); err != nil{
-	err := process(data)
-	if err != nil {
-		fmt.Println("Error:", err)
+	// err := process(data)
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// 	return
+	// }
+	// fmt.Println("Data Process successfully")
+
+
+	//error interface of builtin package
+	// err1 := eprocess()
+	// if err1 != nil {
+	// 	fmt.Println(err1)
+	// 	return
+	// }
+	// println("")
+
+	err := readData()
+	if err != nil{
+		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Data Process successfully")
+	fmt.Println("Data read successfully.")
 
+}
+
+type myError struct {
+	message string
+}
+
+func (m *myError) Error() string {
+	return fmt.Sprintf("Error: %s", m.message)
+}
+
+func eprocess() error {
+	return &myError{"Custom error message"}
+}
+
+func readData() error{
+	err := readConfig()
+	if err != nil{
+		return fmt.Errorf("readData: %w", err)
+	}
+	return nil 
+}
+
+func readConfig() error{
+	return errors.New("Config error")
 }
