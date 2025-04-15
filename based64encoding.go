2cb4485d4841fcc51c9ec10c1f21fc43f9a27e73
joss12 <drh374@mail.com>
2025-04-15 05:39:50 -0400

    based64encoding.go

---
+++ based64encoding.go
@@ -0,0 +1,28 @@
+package intermediate
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+
+func main (){
+	data := []byte("He~lo, base64 Encoding");
+
+	//Encoded Based64
+	encoded := base64.StdEncoding.EncodeToString(data)
+	fmt.Println(encoded)
+
+	//Decode from Base64
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil{
+		fmt.Println("Error in decoding:", err)
+		return
+	}
+	fmt.Println("Decoded:", string(decoded))
+
+	// URL: safe, avoid '/' and '+'
+	urlSafeEncoded := base64.URLEncoding.EncodeToString(data)
+	fmt.Println("URL Safe encoded:", urlSafeEncoded)
+
+}
