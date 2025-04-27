a62da9178c233033e8c2cada37320e1d05e842b2
joss12 <drh374@mail.com>
2025-04-27 12:24:16 -0400

    zap_log

---
+++ zap_log.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+
+func main (){
+
+	logger, err:= zap.NewProduction()
+	if err != nil{
+		log.Println("Error initializing Zap logger")
+	}
+
+	defer logger.Sync()
+
+	logger.Info("This is an info message.")
+	logger.Info("User logged in", zap.String("user","eddy"), zap.String("method", "GET"))
+
+}
