553950a6514515edf706ed5efa4f1c98c965df02
joss12 <drh374@mail.com>
2025-05-06 17:47:52 -0400

    channels.go

---
+++ channels.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//to create a channel we use make functions
+	// variable := make(chan type)
+	greeting := make(chan string)
+	greetString := "Hello"
+
+	go func() {
+		greeting <- greetString // blocking because it is continuously trying to receive values, it is ready to receive continuous  flow of data
+		greeting <- "World"
+
+		for _, e := range "abcde" {
+			greeting <- "Alphabet:" + string(e)
+		}
+	}()
+
+	// go func() {
+	// receiver := <-greeting
+	// fmt.Println(receiver)
+	// receiver = <-greeting
+	// fmt.Println(receiver)
+	// }()
+
+	receiver := <-greeting
+	fmt.Println(receiver)
+	receiver = <-greeting
+	fmt.Println(receiver)
+
+	for range 5 {
+		rcvr := <-greeting
+		fmt.Println(rcvr)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("End of program.")
+
+}
